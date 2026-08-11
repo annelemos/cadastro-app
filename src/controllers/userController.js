6f5a2f4bcf48d2f658e29b425a972c4482e4a5ce
Anne <annelemosmacedo@gmail.com>
2026-08-11 18:24:08 -0300

    feat: cria controller para cadastro de usuario

---
+++ src/controllers/userController.js
@@ -0,0 +1,14 @@
+import { criarUsuario } from "../models/userModel.js";
+
+async function cadastrarUsuario(req,res) {
+    const {nome,email,senha} = req.body;
+    try{
+        const resultado = await criarUsuario(nome, email, senha);
+        res.status(201).json({ mensagem: "Usuário criado com sucesso" })
+    }catch(erro){
+        res.status(500).json({ mensagem: "Falha ao tentar criar novo usuário", erro: erro.message });
+    }
+    return 
+}
+
+export { cadastrarUsuario };
