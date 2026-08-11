0085b9e3a11fc429819d668316e1e58b7a708757
Anne <annelemosmacedo@gmail.com>
2026-08-11 18:34:40 -0300

    feat: conecta rotas de usuario ao servidor Express

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import router from "./src/routes/userRoutes.js";
 
 const app = express();
+app.use(express.json());
+app.use('/api', router);
 const port = 3000;
 
 app.get('/', (req, res) => {
