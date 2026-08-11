f2d8326ffa96298fddc5d83f1237680a16af3105
Anne <annelemosmacedo@gmail.com>
2026-08-11 18:28:44 -0300

    feat: cria rota POST para cadastro de usuarios

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { cadastrarUsuario } from '../controllers/userController.js';
+const router = express.Router();
+
+router.post('/usuarios', cadastrarUsuario);
+
+export default router;
