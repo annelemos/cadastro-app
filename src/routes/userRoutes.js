b4b4fa4e8f47dba2271defb0e5f494530a14a6f8
Anne <annelemosmacedo@gmail.com>
2026-08-12 11:14:10 -0300

    feat: adiciona rota GET para listagem de usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { cadastrarUsuario } from '../controllers/userController.js';
+import { cadastrarUsuario, listarUsuariosController } from '../controllers/userController.js';
 const router = express.Router();
 
 router.post('/usuarios', cadastrarUsuario);
+router.get('/usuarios', listarUsuariosController);
 
 export default router;
