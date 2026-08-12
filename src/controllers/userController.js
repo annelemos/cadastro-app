e9f0b214e61ad00b1468d10886c8f862353f828a
Anne <annelemosmacedo@gmail.com>
2026-08-12 10:59:06 -0300

    fix: corrige status code de erro para 400 em caso de falha no cadastro

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,7 @@ async function cadastrarUsuario(req,res) {
         const resultado = await criarUsuario(nome, email, senha);
         res.status(201).json({ mensagem: "Usuário criado com sucesso" })
     }catch(erro){
-        res.status(500).json({ mensagem: "Falha ao tentar criar novo usuário", erro: erro.message });
+        res.status(400).json({ mensagem: "Falha ao tentar criar novo usuário", erro: erro.message });
     }
     return 
 }
