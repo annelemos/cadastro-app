c2f63e3d18836f7c04cd84ea502c489312a30ed1
Anne <annelemosmacedo@gmail.com>
2026-08-14 11:01:00 -0300

    feat: adiciona rotas PUT e DELETE para usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { cadastrarUsuario, listarUsuariosController } from '../controllers/userController.js';
+import { cadastrarUsuario, deletarUsuarioController, editarUsuariosController, listarUsuariosController } from '../controllers/userController.js';
+
 const router = express.Router();
 
 router.post('/usuarios', cadastrarUsuario);
 router.get('/usuarios', listarUsuariosController);
+router.put('/usuarios/:id', editarUsuariosController);
+router.delete('/usuarios/:id', deletarUsuarioController);
 
 export default router;
