7f5ab0c692d70ef049b80f79dcab5bd54e054f2d
Anne <annelemosmacedo@gmail.com>
2026-08-14 10:38:22 -0300

    feat: adiciona funcao para editar usuario no model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,5 +14,12 @@ async function listarUsuarios () {
     const  resultado = await conexao.query('SELECT * FROM usuarios');
     return resultado.rows;
 }
+async function editarUsuario(nome,email,senha,id) {
+    const query = `
+    UPDATE usuarios SET nome = $1, email = $2, senha = $3 WHERE id = $4
+    `;
+    const resultado = await conexao.query(query,[nome,email,senha,id]);
+    return resultado;
+}
 
-export { criarUsuario, listarUsuarios };
+export { criarUsuario, listarUsuarios, editarUsuario };
